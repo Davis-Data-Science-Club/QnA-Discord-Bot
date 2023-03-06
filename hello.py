c8937d3811492caa32d0b419f3171ba4e1cfe6d8
codermei8 <119540082+codermei8@users.noreply.github.com>
2023-03-05 22:13:52 -0800

    Implemented rating system and functionality to ping officers.
    The bot is ready for integration with nltk functions. When a message is sent, the message is stored in a variable which can then be used as an argument within a function that uses nltk to parse and return an output to the user. If the response is null, in this case - if a user types "Freeze!", then the bot pings a specific role which may be able to help them with their question. The ratings are stored in a .txt file with data separated by commas in the following order: question, response, user, and rating.

--- hello.py
+++ hello.py
@@ -1,26 +1,65 @@
 import discord
+import os
+import asyncio
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+ROLE_ID = os.getenv("ROLE_ID")
 
 #Specify what type of access the bot will have. In our case, all.
 intents = discord.Intents.all()
 intents.members = True
 
-#Creates a new discord client
-client = discord.Client(intents=intents)
+#Creates a new discord Bot
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 #Event handler that prints "Logged in" when the discord bot is up and running.
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'Logged in as {bot.user}')
 
-#Event handler that receives a message and replies "hello" to any user input. 
-#It handles conditional logic such that message inputs hi or hello responds hello with the user's discord name. 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+ratings = {}
+
+#placeholder function for nltk portion
+def example_function(input):
+    if input == "Freeze!":
         return
-    if message.content.lower() == "hello" or "hi":
-        await message.channel.send(f"Hello {message.author.name}!")
-    else:
-        await message.channel.send("Hello!")
+    return input
 
-client.run('BOT_TOKEN')
+#Bot command that is triggered using !q, will call the function and output a response. 
+@bot.command()
+async def q(ctx, *, question):
+    response = example_function(question)
+    if response is None:
+        await ctx.send("Sorry, I couldn't find an answer to that question.")
+        role = ctx.guild.get_role(ROLE_ID)
+        await ctx.send("Maybe an " + role.mention + " can help you out.")
+    else:
+        await ctx.send(response)
+        # Ask the user to rate the response
+        await ctx.send("Please rate my response from 1 to 10.")
+        
+        def check(message):
+            return message.author == ctx.author and message.channel == ctx.channel and message.content.isdigit()
+
+        # Wait for the user's rating
+        try:
+            rating = await bot.wait_for("message", check=check, timeout=10.0)
+            rating_value = int(rating.content)
+            if rating_value < 1 or rating_value > 10:
+                await ctx.send("Invalid rating. Please enter a number between 1 and 10.")
+            else:
+                # Store the question, response, and rating
+                ratings[(ctx.author.name, question, response)] = rating_value
+                await ctx.send("Thank you for your rating.")
+        except asyncio.TimeoutError:
+            await ctx.send("You did not enter a rating. Rating will not be recorded.")
+
+        # Write the ratings to a file
+        with open("ratings.txt", "a") as file:
+            file.write(f"{ctx.author.name}, {question}, {response}, {rating_value}\n")
+
+bot.run(DISCORD_TOKEN)
